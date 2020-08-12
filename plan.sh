3540a7414fbb59d24a7b3ce283362b6dd750c43c
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-08-12 10:40:00 -0400

    Add yarn installation

--- plan.sh
+++ plan.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+if [ "$1" = "cdktf" ]; then
+    yarn install
+    cdktf get
+fi
+
 trap 'quit=1' USR1
 
 echo "Reloading terraform plan..."
 
 if [ "$1" = "cdktf" ]; then
-    npm run build
     cdktf diff
 elif [ "$1" = "terraform" ]; then
     terraform init "${2}"
